e0544876bf1ff292af199d9ebbd9292069e3d0cc
mascarenhaswanja <wanjamarly.deoliveiramascarenhas@georgebrown.ca>
2020-04-23 18:00:04 -0400

    rout to heroku

--- routes/chat.js
+++ routes/chat.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const chatController = require('../controllers/chatController');
 
-router.get('/:roomId', auth, chatController.index);
+router.get('/:roomId', chatController.index);
+//router.get('/:roomId', auth, chatController.index);
 
 module.exports = router;
--- routes/rooms.js
+++ routes/rooms.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const auth = require('../middlewares/auth');
 const roomsController = require('../controllers/roomsController');
 
-router.get('/', auth, roomsController.index);
+router.get('/', roomsController.index);
+//router.get('/', auth, roomsController.index);
 
 module.exports = router;
