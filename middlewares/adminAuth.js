4bc57c979c42b57ec6596d5773721b71a3bba9c1
mascarenhaswanja <wanjamarly.deoliveiramascarenhas@georgebrown.ca>
2020-04-27 10:13:25 -0400

    route Heroku

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -6,13 +6,13 @@ exports.checkLogin = (req, res, next) => {
     const adminToken = req.cookies.adminToken;
 
     if (!adminToken) {
-        res.status(401).send({ message: 'Invalid login/password' });
+        res.status(401).send({ message: 'Invalid login/password 1' });
     }
 
     try {
         jwt.verify(adminToken, privateKey);
     } catch (error) {
-        res.status(401).send({ message: 'Invalid login/password' });
+        res.status(401).send({ message: 'Invalid login/password 2' });
     }
 
     next();
